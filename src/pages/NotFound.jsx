95cb41e978bce419135d39a1de09b28236288680
“Nilde <nilde.benedetti@gmail.com>
2026-06-22 17:40:08 +0200

    aggiunta pagina notfound

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function NotFound() {
+    return (
+        <>
+            <div className='notfound'>
+                <div className='container tw-bold text-center mt-5'>
+                    <h1>Something went wrong</h1>
+                    <Link to="/HomePage">
+                        <button className='btn btn-warning'>go back to HomePage'</button>
+                    </Link>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default NotFound;
