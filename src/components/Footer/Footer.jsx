4832129754d2b2f05793ec510a58f6569160cd9b
“Nilde <nilde.benedetti@gmail.com>
2026-06-22 14:53:54 +0200

    aggiurnato wording in inglese

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <footer className="bg-dark text-white py-4 mt-5">
             <div className="container text-center">
-                <p className="mb-0">© 2026 Prometheus Labs - Tutti i diritti riservati</p>
+                <p className="mb-0">© 2026 Prometheus Labs - All rightsreserved</p>
             </div>
             </footer>
     )
