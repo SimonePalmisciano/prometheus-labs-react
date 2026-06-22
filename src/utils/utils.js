26a882388c595e66df249a1afc98661ebef61363
“Nilde <nilde.benedetti@gmail.com>
2026-06-22 22:34:27 +0200

    rimosse variabili da utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,1 @@
-    const SERVER_PORT = import.meta.env.VITE_SERVER_PORT || '3000';
-    const SERVER_URL = import.meta.env.VITE_SERVER_URL || 'localhost';
-    const API_BASE_URL = `http://${SERVER_URL}:${SERVER_PORT}`;
-
-    const utilsVariables = {
-        SERVER_PORT,
-        SERVER_URL,
-        API_BASE_URL
-    }
-
-    export default utilsVariables;
+/** metti quel che vuoi e ti serve  */
