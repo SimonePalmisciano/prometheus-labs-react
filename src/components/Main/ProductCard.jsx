1ced4c9ec5a6c6efaea2587aff2a0dd48507df47
Ivan Caldarella <gamein3x@gmail.com>
2026-06-23 16:20:01 +0200

    In productCard: Immagine selezionabile per detail, rimosso bottone;

--- src/components/Main/ProductCard.jsx
+++ src/components/Main/ProductCard.jsx
@@ -4,20 +4,21 @@ function ProductCard({ prodotto }) {
   return (
     <div className="col">
       <div className="card h-100">
-        <img
-          src={`http://localhost:3000${prodotto.imgMain}`}
-          className="card-img-top"
-          alt={prodotto.name}
-        />
+        <Link to={`/products/${prodotto.slug}`}>
+          <img
+            src={`http://localhost:3000${prodotto.imgMain}`}
+            className="card-img-top"
+            alt={prodotto.name}
+          />
+        </Link>
         <div className="card-body">
           <h5 className="card-title">{prodotto.name}</h5>
           <p className="card-text">{prodotto.shortDescription}</p>
         </div>
         <div className="card-footer d-flex justify-content-between align-items-center">
           <span className="fw-bold">€ {prodotto.price}</span>
-          <Link to={`/ProductDetail/${prodotto.slug}`}>
-          <i className="bi bi-arrow-right-circle-fill fs-2"></i>
-          </Link>
+
+
         </div>
       </div>
     </div>
