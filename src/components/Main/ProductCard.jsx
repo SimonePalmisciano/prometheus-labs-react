08a00f28fcf064b509585525b077ab1443f97b59
Ivan Caldarella <gamein3x@gmail.com>
2026-06-24 10:48:33 +0200

    Display cards modificato, cambiato "prodotti" e "prodotto" in inglese in card e parent

--- src/components/Main/ProductCard.jsx
+++ src/components/Main/ProductCard.jsx
@@ -1,22 +1,35 @@
+import { useState } from "react";
 import { Link } from "react-router"
-// Componente card singolo prodotto
-function ProductCard({ prodotto }) {
+// Componente card singolo product
+function ProductCard({ product }) {
+  const [expanded, setExpanded] = useState(false);
   return (
-    <div className="col">
+    <div className="col-12 col-md-6 col-lg">
       <div className="card h-100">
-        <Link to={`/products/${prodotto.slug}`}>
+        <Link to={`/products/${product.slug}`}>
           <img
-            src={`http://localhost:3000${prodotto.imgMain}`}
+            src={`http://localhost:3000${product.imgMain}`}
             className="card-img-top"
-            alt={prodotto.name}
+            alt={product.name}
           />
         </Link>
         <div className="card-body">
-          <h5 className="card-title">{prodotto.name}</h5>
-          <p className="card-text">{prodotto.shortDescription}</p>
+          <h5 className="card-title">{product.name}</h5>
+          <p className="card-text">
+            {expanded
+              ? product.shortDescription
+              : product.shortDescription.slice(0, 75) + "..."
+            }
+          </p>
+          <button
+            className="btn btn-link text-primary p-0"
+            onClick={() => setExpanded(!expanded)}
+          >
+            {expanded ? "Show less ▲" : "Read more ▼"}
+          </button>
         </div>
         <div className="card-footer d-flex justify-content-between align-items-center">
-          <span className="fw-bold">€ {prodotto.price}</span>
+          <span className="fw-bold">€ {product.price}</span>
           <i className="bi bi-cart3"></i>
         </div>
       </div>
