0ba3447dd0225ce832af18a32144bf3748757742
Amirali Rezaei <amiralirezaie169@gmail.com>
2026-07-01 08:46:53 +0200

    feat(footer): add navigation section and improve layout

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import "../../styles/footer.css";
 import { FaInstagram, FaYoutube, FaFacebook, FaLinkedin } from "react-icons/fa";
-
+import { Link } from "react-router";
 
 export default function Footer() {
     return (
@@ -22,42 +22,26 @@ export default function Footer() {
                     <p>Address: Via Roma 12, Milan 20100 - Italy</p>
                 </div>
 
+                {/* NAVIGATION */}
+                <div className="footer-nav">
+                    <h4>Navigation</h4>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/about">About Us</Link></li>
+                        <li><Link to="/products">Products</Link></li>
+                    </ul>
+                </div>
+
                 {/* SOCIAL */}
                 <div className="footer-social">
                     <h4>Social</h4>
                     <ul>
-
-                        <li>
-                            <a href="#">
-                                <FaInstagram className="social-icon" />
-                                Instagram
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href="#">
-                                <FaYoutube className="social-icon" />
-                                YouTube
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href="#">
-                                <FaFacebook className="social-icon" />
-                                Facebook
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href="#">
-                                <FaLinkedin className="social-icon" />
-                                LinkedIn
-                            </a>
-                        </li>
-
+                        <li><a href="#"><FaInstagram className="social-icon" />Instagram</a></li>
+                        <li><a href="#"><FaYoutube className="social-icon" />YouTube</a></li>
+                        <li><a href="#"><FaFacebook className="social-icon" />Facebook</a></li>
+                        <li><a href="#"><FaLinkedin className="social-icon" />LinkedIn</a></li>
                     </ul>
                 </div>
-
 
             </div>
 
